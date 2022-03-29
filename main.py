2788dd834ff351f9c39f4c8ee57bc568632f0cdf
Avelino Jacinto <avelino@planet.net>
2022-03-29 19:22:56 -0400

    Organizing shit and setting better variables

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ from selenium import webdriver
 from selenium.webdriver import Keys
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
+#Must download chrome driver and add it to the above location or copy its location to the path variable
 driver = webdriver.Chrome(PATH)
-driver.get("https://voice.planet.net/pbx/login.php")
+
+#Mirta Link
+website = "https://voice.planet.net/pbx/login.php"
+driver.get(website)
+#Credentials
+username = "avelino@planet.net"
+password = "LssVPdJm"
+
 search = driver.find_element_by_name("username")
-search.send_keys("avelino@planet.net")
+search.send_keys(username)
 search = driver.find_element_by_name("password")
-search.send_keys("LssVPdJm")
+search.send_keys(password)
 search.send_keys(Keys.RETURN)
 search = driver.find_element_by_name("2facode")
 print("Break")
-twoFa = input("Please Enter twoFa:    ")
+twoFa = input("Please Enter twoFa ---> ")
 search.send_keys(twoFa)
 search.send_keys(Keys.RETURN)
 
